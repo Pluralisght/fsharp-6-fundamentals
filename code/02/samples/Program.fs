3e51af7cf1ef5e22b5aeafee88826bde3f83ac12
mheydt <mike@heydt.org>
2022-03-20 23:17:50 -0600

    After weekend of edits

---
+++ code/02/samples/Program.fs
@@ -0,0 +1,7 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let argv = System.Environment.GetCommandLineArgs()
+
+for arg in argv do
+    printfn "%A" arg
